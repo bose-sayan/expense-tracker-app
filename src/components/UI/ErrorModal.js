bfe3eb393fec62a9f76ec1ca02a83efb1323e5ad
Sayan Bose <sayan.bose.lmg.2000@gmail.com>
2022-12-13 19:17:54 +0530

    Used React Fragments, Portals to structure the HTML sematically

--- src/components/UI/ErrorModal.js
+++ src/components/UI/ErrorModal.js
@@ -1,24 +1,43 @@
 import React from "react";
-
+import ReactDOM from "react-dom";
 import Card from "./Card";
 import Button from "./Button";
 import classes from "./ErrorModal.module.css";
 
 const ErrorModal = (props) => {
-  return (
-    <div className={classes.backdrop}>
-      <Card className={classes.modal} style={{ background: "white" }}>
-        <header className={classes.header}>
-          <h2>{props.title}</h2>
-        </header>
-        <div className={classes.content}>
-          <p>{props.message}</p>
-        </div>
-        <footer className={classes.actions}>
-          <Button onClick={props.onConfirm}>Okay</Button>
-        </footer>
-      </Card>
+  const Backdrop = (props) => (
+    <div className={classes.backdrop} onClick={props.onConfirm}>
+      {" "}
     </div>
+  );
+  const Modal = (props) => (
+    <Card className={classes.modal} style={{ background: "white" }}>
+      <header className={classes.header}>
+        <h2>{props.title}</h2>
+      </header>
+      <div className={classes.content}>
+        <p>{props.message}</p>
+      </div>
+      <footer className={classes.actions}>
+        <Button onClick={props.onConfirm}>Okay</Button>
+      </footer>
+    </Card>
+  );
+  return (
+    <React.Fragment>
+      {ReactDOM.createPortal(
+        <Backdrop onConfirm={props.onConfirm} />,
+        document.getElementById("backdrop-root")
+      )}
+      {ReactDOM.createPortal(
+        <Modal
+          title={props.title}
+          message={props.message}
+          onConfirm={props.onConfirm}
+        />,
+        document.getElementById("modal-root")
+      )}
+    </React.Fragment>
   );
 };
 export default ErrorModal;
